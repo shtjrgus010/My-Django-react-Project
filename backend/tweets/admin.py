bfe1ed7eccb33dfc11302b898352a4546104d7f0
노석현 <shtjrgus010@github.com>
2024-11-05 16:24:34 +0900

    feat: Day 9 add search and filter

--- backend/tweets/admin.py
+++ backend/tweets/admin.py
@@ -1,17 +1,68 @@
 from django.contrib import admin
 from .models import Tweet, Like
 
+class WordFilter(admin.SimpleListFilter):
+    
+    title = "Filter by Elon"
+    
+    parameter_name = "elon_musk"
+    
+    def lookups(self, request, model_admin):
+        return [
+            ("elon_musk", "Contains Elon Musk"),
+            ("not_elon_musk", "Not contain Elon Musk"),
+        ]
+        
+    def queryset(self, request, payloads):
+        word = self.value()
+        if word == "elon_musk":
+            return payloads.filter(payload__contains="Elon Musk")
+        elif word == "not_elon_musk":
+            return payloads.exclude(payload__contains="Elon Musk")
+        else:
+            return payloads
+
 # Register your models here.
 @admin.register(Tweet)
 class TweetAdmin(admin.ModelAdmin):
-    list_display = ("user", "short_payload", "like_count", "created_at", "updated_at")
+    list_display = (
+        "user",
+        "short_payload",
+        "like_count",
+        "created_at",
+        "updated_at",
+    )
+    
+    list_filter = (
+        WordFilter,
+        "created_at",
+    )
+    
+    search_fields = (
+        "payload",
+        "user__username",
+    )
+
     
     def short_payload(self, obj):
         return obj.payload[:10]
-    short_payload.short_description="payload"
-    
-    
+
+    short_payload.short_description = "payload"
+
 
 @admin.register(Like)
 class LikeAdmin(admin.ModelAdmin):
-    list_display = ("user", "tweet", "created_at", "updated_at")
+    list_display = (
+        "user",
+        "tweet",
+        "created_at",
+        "updated_at",
+    )
+    
+    list_filter = (
+        "created_at",
+    )
+    
+    search_fields = (
+        "user__username",
+    )
--- backend/tweets/models.py
+++ backend/tweets/models.py
@@ -1,35 +1,38 @@
 from django.db import models
 from common.models import CommonModel
 
+
 # Create your models here.
 class Tweet(CommonModel):
-   """Model Definition for Tweet"""
-   
-   payload = models.CharField(max_length=180,)
-   user = models.ForeignKey(
-      "users.User",
-      on_delete=models.CASCADE,
-   )
+    """Model Definition for Tweet"""
 
-   def __str__(self):
-      return f"{self.user} Tweet ...{self.payload[:10]}..."
-   
-   def like_count(self):
-      return self.like.count()
-   
+    payload = models.CharField(
+        max_length=180,
+    )
+    user = models.ForeignKey(
+        "users.User",
+        on_delete=models.CASCADE,
+    )
+
+    def __str__(self):
+        return f"{self.user} Tweet ...{self.payload[:10]}..."
+
+    def like_count(self):
+        return self.like.count()
+
+
 class Like(CommonModel):
-   """Model Definition for Like"""
-   
-   user = models.ForeignKey(
-      "users.User",
-      on_delete=models.CASCADE,
-      
-   )
-   tweet = models.ForeignKey(
-      "tweets.Tweet",
-      on_delete=models.CASCADE,
-      related_name="like",
-   )
-   
-   def __str__(self):
-      return f"Like by {self.user} on Tweet {self.tweet.id}"
+    """Model Definition for Like"""
+
+    user = models.ForeignKey(
+        "users.User",
+        on_delete=models.CASCADE,
+    )
+    tweet = models.ForeignKey(
+        "tweets.Tweet",
+        on_delete=models.CASCADE,
+        related_name="like",
+    )
+
+    def __str__(self):
+        return f"Like by {self.user} on Tweet {self.tweet.id}"
